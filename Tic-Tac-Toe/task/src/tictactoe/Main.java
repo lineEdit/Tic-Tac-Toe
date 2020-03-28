d0a3b3b47c12fb990f2506c33d11c45be7942bcb
lineedit <lineedit@gmail.com>
2020-03-28 16:41:17 +0500

    Stage 1, end.

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -3,5 +3,9 @@ package tictactoe;
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        System.out.println("" +
+                "X O X\n" +
+                "O X O\n" +
+                "X X O ");
     }
 }
