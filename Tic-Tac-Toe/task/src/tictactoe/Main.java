a40da91447f7d3f7e0642efa5071f8da146081ec
lineedit <lineedit@gmail.com>
2020-03-28 16:54:47 +0500

    Stage 2, variant using StringBuilder

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,11 +1,24 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("" +
-                "X O X\n" +
-                "O X O\n" +
-                "X X O ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter cells: ");
+        String string = scanner.next();
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("---------\n");
+        stringBuilder.append("|\t");
+        stringBuilder.append(string, 0, 3);
+        stringBuilder.append("\t|\n");
+        stringBuilder.append("|\t");
+        stringBuilder.append(string, 3, 6);
+        stringBuilder.append("\t|\n");
+        stringBuilder.append("|\t");
+        stringBuilder.append(string, 6, 9);
+        stringBuilder.append("\t|\n");
+        stringBuilder.append("---------");
+        System.out.println(stringBuilder.toString());
     }
 }
