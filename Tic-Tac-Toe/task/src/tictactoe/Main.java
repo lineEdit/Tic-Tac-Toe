8023860575912bd223bbbadbdaa8009ddf5c16a9
lineedit <lineedit@gmail.com>
2020-03-28 18:01:51 +0500

    Stage 2, testing using for xRow, oRow, zRow

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner("XXXOO__O_");
         System.out.println("Enter cells: ");
         char[] chars = scanner.next().toCharArray();
         show(chars);
+        stateGame(chars);
     }
 
     private static void show(char[] chars) {
@@ -28,5 +30,24 @@ public class Main {
                 ,chars[8]
         );
         System.out.println(outString);
+    }
+
+    public static void stateGame(char[] chars) {
+        int[] xRow = new int[3];
+        int[] oRow = new int[3];
+        int[] zRow = new int[3];
+        for (int i = 0; i < 3; ++i) {
+            for (int j = i * 3; j < (i + 1) * 3; ++j) {
+                if (chars[j] == 'X') {
+                    xRow[i]++;
+                }
+                if (chars[j] == 'O') {
+                    oRow[i]++;
+                }
+                if (chars[j] == '_') {
+                    zRow[i]++;
+                }
+            }
+        }
     }
 }
